eb3735c3497651fffa1700fad9dd30c074dfe162
adamflitney <adam.flitney@gmail.com>
2019-03-14 19:22:37 +0000

    second test passing

--- courier.js
+++ courier.js
@@ -1,6 +1,12 @@
 
 const calculateOrderShippingCost = (order) => {
-    
+    let resultArray = [];
+
+    order.forEach(parcel => {
+        resultArray.push(0);
+    });
+
+    return resultArray;
 }
 
 module.exports = {
