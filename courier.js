514adf943c4641eba65924b1301f66caad0279ef
adamflitney <adam.flitney@gmail.com>
2019-03-14 19:40:56 +0000

    fifth test passing

--- courier.js
+++ courier.js
@@ -3,12 +3,16 @@ const calculateOrderShippingCost = (order) => {
     let resultArray = [];
 
     order.forEach(parcel => {
-        if (parcel.width < 50 && parcel.height < 50 && parcel.depth < 50) {
-            if (parcel.width < 10 && parcel.height < 10 && parcel.depth < 10) {
-                resultArray.push(3);
+        if (parcel.width < 100 && parcel.height < 100 && parcel.depth < 100) {
+            if (parcel.width < 50 && parcel.height < 50 && parcel.depth < 50) {
+                if (parcel.width < 10 && parcel.height < 10 && parcel.depth < 10) {
+                    resultArray.push(3);
+                    return;
+                }
+                resultArray.push(8);
                 return;
             }
-            resultArray.push(8);
+            resultArray.push(15);
             return;
         }
         resultArray.push(0);
