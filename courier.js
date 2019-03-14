7945843f6ec446e982e71c22945953108a0c1efb
adamflitney <adam.flitney@gmail.com>
2019-03-14 20:32:05 +0000

    12th test passing

--- courier.js
+++ courier.js
@@ -3,7 +3,7 @@ const calculateOrderShippingCost = (order, speedy = false) => {
     let resultArray = [];
 
     order.forEach(parcel => {
-        if(parcel.width >= 100 || parcel.height >= 100 || parcel.depth >= 100) {
+        if (parcel.width >= 100 || parcel.height >= 100 || parcel.depth >= 100) {
             resultArray.push(25);
             return;
         }
@@ -11,25 +11,29 @@ const calculateOrderShippingCost = (order, speedy = false) => {
             if (parcel.width < 50 && parcel.height < 50 && parcel.depth < 50) {
                 if (parcel.width < 10 && parcel.height < 10 && parcel.depth < 10) {
                     let extra = 0;
-                    if(parcel.weight > 1) {
+                    if (parcel.weight > 1) {
                         extra = (parcel.weight - 1) * 2;
                     }
                     resultArray.push(3 + extra);
                     return;
                 }
                 let extra = 0;
-                if(parcel.weight > 3) {
+                if (parcel.weight > 3) {
                     extra = (parcel.weight - 3) * 2;
                 }
                 resultArray.push(8 + extra);
                 return;
             }
-            resultArray.push(15);
+            let extra = 0;
+            if (parcel.weight > 6) {
+                extra = (parcel.weight - 6) * 2;
+            }
+            resultArray.push(15 + extra);
             return;
         }
         resultArray.push(0);
     });
-    if(speedy) {
+    if (speedy) {
         const subTotalCost = resultArray.reduce((acc, cur) => acc + cur);
         resultArray.push(subTotalCost);
     }
