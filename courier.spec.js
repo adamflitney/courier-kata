5713e580f433bbd9cd324c48946c8e65e01df5ac
adamflitney <adam.flitney@gmail.com>
2019-03-14 19:28:46 +0000

    third test

--- courier.spec.js
+++ courier.spec.js
@@ -18,6 +18,11 @@ describe('calculateOrderShippingCost', () => {
             depth: 100
         }
     ];
+    const smallParcel = {
+        height: 9,
+        width: 8,
+        depth: 9
+    }
     it('should accept an array of parcel objects', () => {
         expect(() => {courier.calculateOrderShippingCost(order)}).not.toThrow();
     });
@@ -25,5 +30,9 @@ describe('calculateOrderShippingCost', () => {
     it('should return an equal size array of costs', () => {
         expect(courier.calculateOrderShippingCost(order)).toHaveLength(3);
     });
+
+    it('should return a cost of $3 for a small parcel (all dimensions < 10cm)', () => {
+        expect(courier.calculateOrderShippingCost([smallParcel])).toEqual([3]);
+    })
 
 });
