fe21a96726a3aea7cbcf2a49e16c881f5997702d
adamflitney <adam.flitney@gmail.com>
2019-03-14 19:31:31 +0000

    third test passed

--- courier.js
+++ courier.js
@@ -3,6 +3,10 @@ const calculateOrderShippingCost = (order) => {
     let resultArray = [];
 
     order.forEach(parcel => {
+        if(parcel.width < 10 && parcel.height < 10 && parcel.depth < 10){
+            resultArray.push(3);
+            return;
+        }
         resultArray.push(0);
     });
 
